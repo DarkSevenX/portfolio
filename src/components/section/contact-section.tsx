6b9c5e372c2e1873c966562dd3ef8049a373f3de
DarkSevenX <nargumedor@unicartagena.edu.co>
2026-07-17 16:55:29 -0500

    ultimos retoques equisde

--- src/components/section/contact-section.tsx
+++ src/components/section/contact-section.tsx
@@ -6,7 +6,7 @@ export default function ContactSection() {
   return (
     <div className="border rounded-xl p-10 relative">
       <div className="absolute -top-4 border bg-primary z-10 rounded-xl px-4 py-1 left-1/2 -translate-x-1/2">
-        <span className="text-background text-sm font-medium">Contact</span>
+        <span className="text-background text-sm font-medium">Contacto</span>
       </div>
       <div className="absolute inset-0 top-0 left-0 right-0 h-1/2 rounded-xl overflow-hidden">
         <FlickeringGrid
@@ -21,21 +21,25 @@ export default function ContactSection() {
       </div>
       <div className="relative flex flex-col items-center gap-4 text-center">
         <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-          Get in Touch
+          ¿Tienes un proyecto en mente?
         </h2>
         <p className="mx-auto max-w-lg text-muted-foreground text-balance">
-          Want to chat? Just shoot me a dm{" "}
+          Me encantaría escuchar sobre él y colaborar contigo{" "}
           {/* <Link
             href={DATA.contact.social.X.url}
             target="_blank"
             rel="noopener noreferrer"
             className="text-blue-500 hover:underline underline-offset-4 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 rounded-sm"
           > */}
-          with a direct question on twitter
+
           {/* </Link>{" "} */}
-          and I&apos;ll respond whenever I can. I will ignore all
-          soliciting.
         </p>
+        <Link
+          href="mailto:nelsonargumedo2809@gmail.com"
+          className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring"
+        >
+          Enviar Email
+        </Link>
       </div>
     </div>
   );
